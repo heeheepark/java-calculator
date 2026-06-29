8f8f48c7a801e1bf5f2b327bfc34f0d7108a1bda
heeheepark <pjh9641@gmail.com>
2026-06-29 12:18:37 +0900

    feat: 원의 넓이 구하기 로직 추가

--- src/Main.java
+++ src/Main.java
@@ -6,45 +6,70 @@ public class Main {
     private static final List<String> CALC_TYPES = List.of("+", "-", "*", "/");
 
     public static void main(String[] args) {
-        List<Integer> values = new ArrayList<>();
-
+        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = scanner.nextInt();
-            validateNumber(num1);
+            System.out.println("실행 타입을 선택해 주세요. [1. 원의 넓이 구하기 , 2. 사칙연산]");
 
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = scanner.nextInt();
-            validateNumber(num2);
-            scanner.nextLine();
+            int type = scanner.nextInt();
+            switch (type) {
+                case 1:
+                    System.out.print("반지름을 입력하세요: ");
+                    double radius = scanner.nextDouble();
+                    double circleArea = calculator.calculateCircleArea(radius);
+                    System.out.println("원의 넓이: " + circleArea);
+                    scanner.nextLine();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            String calcType = scanner.nextLine();
-            validateCalcType(calcType);
+                    calculator.addCircleArea(circleArea);
 
-            // 사칙연산 수행
-            int result = calculate(calcType, num1, num2);
-            System.out.println("결과: " + result);
+                    List<Double> circleAreas = calculator.getCircleAreas();
+                    for (Double area : circleAreas) {
+                        System.out.println(area);
+                    }
 
-            if (values.size() >= 10) {
-                System.out.println("가장 먼저 연산된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                String removeCommand = scanner.nextLine();
+                    break;
+                case 2:
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    int num1 = scanner.nextInt();
+                    validateNumber(num1);
 
-                if ("remove".equals(removeCommand)) {
-                    values.remove(0);
-                }
-            }
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    int num2 = scanner.nextInt();
+                    validateNumber(num2);
+                    scanner.nextLine();
 
-            values.add(result);
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    String calcType = scanner.nextLine();
+                    validateCalcType(calcType);
 
-            System.out.println("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiryCommand = scanner.nextLine();
-            if ("inquiry".equals(inquiryCommand)) {
-                for (Integer value : values) {
-                    System.out.println(value);
-                }
+                    // 사칙연산 수행
+                    int result = calculator.calculate(calcType, num1, num2);
+                    System.out.println("결과: " + result);
+
+                    if (calculator.getValuesSize() >= 10) {
+                        System.out.println("가장 먼저 연산된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                        String removeCommand = scanner.nextLine();
+
+                        if ("remove".equals(removeCommand)) {
+                            calculator.removeFirstValue();
+                        }
+                    }
+
+                    calculator.addValue(result);
+
+                    System.out.println("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    String inquiryCommand = scanner.nextLine();
+                    if ("inquiry".equals(inquiryCommand)) {
+                        List<Integer> values = calculator.getValues();
+                        for (Integer value : values) {
+                            System.out.println(value);
+                        }
+                    }
+
+                    break;
+                default:
+                    throw new IllegalArgumentException("표시된 항목 중 선택해주세요.");
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
@@ -55,28 +80,6 @@ public class Main {
         }
 
         scanner.close();
-    }
-
-    private static int calculate(String calcType, int num1, int num2) {
-        switch (calcType) {
-            case "+":
-                return num1 + num2;
-            case "-":
-                return num1 - num2;
-            case "*":
-                return num1 * num2;
-            case "/":
-                validateDivisionByZero(num2);
-                return num1 / num2;
-            default:
-                throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
-        }
-    }
-
-    private static void validateDivisionByZero(int divisor) {
-        if (divisor == 0) {
-            throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-        }
     }
 
     private static void validateCalcType(String calcType) {
